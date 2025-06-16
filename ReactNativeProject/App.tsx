fd444146f9d19a0f07b63c9faa09ac4124a7da56
Khaled Akram <khaled134-akram@LIVE.COM>
2025-06-16 18:29:24 +0300

    Task 29: Flatlist & Modal

--- ReactNativeProject/App.tsx
+++ ReactNativeProject/App.tsx
@@ -20,8 +20,9 @@ import Task24 from './Tasks/Task24';
 import Task25 from './Tasks/Task25';
 import Task26 from './Tasks/Task26';
 import Task27 from './Tasks/Task27';
-*/
 import Task28 from './Tasks/Task28';
+*/
+import Task29 from './Tasks/Task29';
 
 function App(): React.JSX.Element {
 
@@ -39,8 +40,10 @@ function App(): React.JSX.Element {
     <Task25 />
     <Task26 />
     <Task27 />
-    */
     <Task28 />
+    */
+    <Task29 />
+
   );
 }
 
